c175be66514e0b63b56784eb58eb01462a4dac23
Uzair Aslam <uzairaslam990@gmail.com>
2024-11-08 05:48:35 -0800

    validation of form

--- app/page.js
+++ app/page.js
@@ -68,7 +68,37 @@ export default function Home() {
         </div>
         <button onClick={OnClickHandler} className="bg-blue-600 text-white rounded-xl font-semibold px-10 py-2">Submit</button>
       </form>
-     
+      {
+        error.map((item,i)=>{
+          return <div key={i} className="text-2xl text-red-500">
+                {item}
+          </div>
+        })
+      }
+      <div className='mt-10 overflow-x-auto '>
+        {contactList.length>0 &&
+            <table className='min-w-full'>
+                <thead>
+                <tr>
+                    <th className='px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-lg leading-4 font-medium text-gray-600 uppercase '>Name</th>
+                    <th className='px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-lg leading-4 font-medium text-gray-600 uppercase '>Phone Number</th>
+                    <th className='px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-lg leading-4 font-medium text-gray-600 uppercase '>Email</th>
+                    <th className='px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-lg leading-4 font-medium text-gray-600 uppercase '>Message</th>
+                    <th className='px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-lg leading-4 font-medium text-gray-600 uppercase '>CNIC</th>
+                    </tr>
+                </thead>
+                <tbody>
+                { contactList.map((element ,i)=>{
+                    return(<tr key={i} >
+                    <td className='px-6 py-3 border-b text-lg border-gray-300 text-left leading-4 text-gray-600'>{element.name}</td>
+                    <td className='px-6 py-3 border-b text-lg border-gray-300 text-left leading-4 text-gray-600'>{element.phone}</td>
+                    <td className='px-6 py-3 border-b text-lg border-gray-300 text-left leading-4 text-gray-600'>{element.email}</td>
+                    <td className='px-6 py-3 border-b text-lg border-gray-300 text-left leading-4 text-gray-600'>{element.message}</td>
+                    <td className='px-6 py-3 border-b text-lg border-gray-300 text-left leading-4 text-gray-600'>{element.cnic}</td>
+                  </tr>)})}
+                </tbody>
+            </table>}
+            </div>
     </div>
   );
 }
